8c4a691f122dfdde27c055c1e1ba2089f37c2ddb
AlisherU <alisherrahmonov@gmail.com>
2021-08-09 15:08:07 +0900

    Complete basic input page

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
+from datetime import timezone
 from flask import Flask, render_template,url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mylib.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+#Database tables
 class Book(db.Model):
-    id = db.Column('db.Integer', primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(300), nullable=False)
+    description = db.Column(db.Text)
+    author = db.Column(db.String(100), nullable=False)
+    genre = db.Column(db.String(100))
+    language = db.Column(db.String(100))
+    finished_reading = db.Column(db.Boolean, default=False)
+    purchase_date = db.Column(db.DateTime)
+    price = db.Column(db.Integer)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __repr__(self):
+        return  '<Book %r>' % self.id
+
 
 #Create  a  route 
 @app.route('/')
@@ -24,10 +41,6 @@ def addbook():
 def store():
     return render_template("store.html")
 
-
-@app.route('/user/<string:name>/<int:id>')
-def user(name, id):
-    return "User Page:" + name + " - " + str(id)
 
 
 if __name__ == '__main__':
